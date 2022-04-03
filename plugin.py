d6dfbc6062f1faae4e1c13576c9f3dd98587cca8
Nikita Ourazbaev <nikita.u@me.com>
2022-04-03 08:08:52 -0400

    Remove redundant scope check

--- plugin.py
+++ plugin.py
@@ -12,21 +12,21 @@ class LiquidHandleSpaceCommand(sublime_plugin.TextCommand):
 
             view = self.view
 
-            if view.match_selector(0, "text.html.liquid"):
-                selectedRegions = view.sel()
+            selectedRegions = view.sel()
 
-                for region in selectedRegions:
-                    prevThreeCharacters = view.substr(sublime.Region(region.a - 4, region.a - 1))
-                    prevTwoCharacters = view.substr(sublime.Region(region.a - 3, region.a - 1))
+            for region in selectedRegions:
+                prevThreeCharacters = view.substr(sublime.Region(region.a - 4, region.a - 1))
+                prevTwoCharacters = view.substr(sublime.Region(region.a - 3, region.a - 1))
 
-                    if prevThreeCharacters == '{%-' or prevThreeCharacters == '{{-' or prevTwoCharacters == '{%' or prevTwoCharacters == '{{':
-                        nextThreeCharacters = view.substr(sublime.Region(region.a, region.a + 3))
-                        nextTwoCharacters = view.substr(sublime.Region(region.a, region.a + 2))
+                if prevThreeCharacters == '{%-' or prevThreeCharacters == '{{-' or prevTwoCharacters == '{%' or prevTwoCharacters == '{{':
+                    nextThreeCharacters = view.substr(sublime.Region(region.a, region.a + 3))
+                    nextTwoCharacters = view.substr(sublime.Region(region.a, region.a + 2))
 
-                        if nextTwoCharacters == '%}' or nextTwoCharacters == '}}' or nextThreeCharacters == '-%}' or nextThreeCharacters == '-}}':
-                            view.run_command("insert", {"characters": ' '})
-                            for x in range(0, 1):
-                                view.run_command("move", {"by": "characters", "forward": False})
+                    if nextTwoCharacters == '%}' or nextTwoCharacters == '}}' or nextThreeCharacters == '-%}' or nextThreeCharacters == '-}}':
+                        view.run_command("insert", {"characters": ' '})
+                        for x in range(0, 1):
+                            view.run_command("move", {"by": "characters", "forward": False})
+
 
 
 class LiquidHandlePercentCommand(sublime_plugin.TextCommand):
@@ -38,24 +38,24 @@ class LiquidHandlePercentCommand(sublime_plugin.TextCommand):
 
             view = self.view
 
-            if view.match_selector(0, "text.html.liquid"):
-                selectedRegions = view.sel()
+            selectedRegions = view.sel()
 
-                for region in selectedRegions:
-                    prevCharacter = view.substr(sublime.Region(region.a - 2, region.a - 1))
+            for region in selectedRegions:
+                prevCharacter = view.substr(sublime.Region(region.a - 2, region.a - 1))
 
-                    lineContents = view.substr(view.line(view.sel()[0]))
+                lineContents = view.substr(view.line(view.sel()[0]))
 
-                    if prevCharacter == '{':
-                        nextChar = view.substr(sublime.Region(region.a, region.a + 1))
-                        if nextChar == '}':
-                            view.run_command("insert", {"characters": '%'})
-                            for x in range(0, 1):
-                                view.run_command("move", {"by": "characters", "forward": False})
-                        elif not '%}' in lineContents:
-                            view.run_command("insert", {"characters": '%}'})
-                            for x in range(0, 2):
-                                view.run_command("move", {"by": "characters", "forward": False})
+                if prevCharacter == '{':
+                    nextChar = view.substr(sublime.Region(region.a, region.a + 1))
+                    if nextChar == '}':
+                        view.run_command("insert", {"characters": '%'})
+                        for x in range(0, 1):
+                            view.run_command("move", {"by": "characters", "forward": False})
+                    elif not '%}' in lineContents:
+                        view.run_command("insert", {"characters": '%}'})
+                        for x in range(0, 2):
+                            view.run_command("move", {"by": "characters", "forward": False})
+
 
 
 class LiquidHandleMinusCommand(sublime_plugin.TextCommand):
@@ -67,19 +67,19 @@ class LiquidHandleMinusCommand(sublime_plugin.TextCommand):
 
             view = self.view
 
-            if view.match_selector(0, "text.html.liquid"):
-                selectedRegions = view.sel()
+            selectedRegions = view.sel()
 
-                for region in selectedRegions:
-                    prevTwoCharacters = view.substr(sublime.Region(region.a - 3, region.a - 1))
+            for region in selectedRegions:
+                prevTwoCharacters = view.substr(sublime.Region(region.a - 3, region.a - 1))
 
-                    if prevTwoCharacters == '{%' or prevTwoCharacters == '{{':
-                        nextTwoCharacters = view.substr(sublime.Region(region.a, region.a + 2))
+                if prevTwoCharacters == '{%' or prevTwoCharacters == '{{':
+                    nextTwoCharacters = view.substr(sublime.Region(region.a, region.a + 2))
 
-                        if nextTwoCharacters == '%}' or nextTwoCharacters == '}}':
-                            view.run_command("insert", {"characters": '-'})
-                            for x in range(0, 1):
-                                view.run_command("move", {"by": "characters", "forward": False})
+                    if nextTwoCharacters == '%}' or nextTwoCharacters == '}}':
+                        view.run_command("insert", {"characters": '-'})
+                        for x in range(0, 1):
+                            view.run_command("move", {"by": "characters", "forward": False})
+
 
 
 class LiquidHandleBraceCommand(sublime_plugin.TextCommand):
@@ -91,17 +91,16 @@ class LiquidHandleBraceCommand(sublime_plugin.TextCommand):
 
             view = self.view
 
-            if view.match_selector(0, "text.html.liquid"):
-                selectedRegions = view.sel()
+            selectedRegions = view.sel()
 
-                for region in selectedRegions:
-                    prevCharacter = view.substr(sublime.Region(region.a - 2, region.a - 1))
+            for region in selectedRegions:
+                prevCharacter = view.substr(sublime.Region(region.a - 2, region.a - 1))
 
-                    if prevCharacter == '{':
-                        nextCharacter = view.substr(sublime.Region(region.a, region.a + 1))
+                if prevCharacter == '{':
+                    nextCharacter = view.substr(sublime.Region(region.a, region.a + 1))
 
-                        if nextCharacter != '}':
-                            view.run_command("insert", {"characters": '}}'})
-                            for x in range(0, 2):
-                                view.run_command("move", {"by": "characters", "forward": False})
+                    if nextCharacter != '}':
+                        view.run_command("insert", {"characters": '}}'})
+                        for x in range(0, 2):
+                            view.run_command("move", {"by": "characters", "forward": False})
 
